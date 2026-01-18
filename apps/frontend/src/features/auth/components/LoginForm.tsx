32fdcd18291977f5f1de5bd36bb72a23e6c65b3f
Chaewon <pcwadarong@naver.com>
2026-01-18 21:41:05 +0900

    refactor: 효용이 없는 useAuth를 삭제하고, 메모이제이션으로 최적화

--- apps/frontend/src/features/auth/components/LoginForm.tsx
+++ apps/frontend/src/features/auth/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { css, useTheme } from '@emotion/react';
-
+import { memo } from 'react';
 import { Button } from '@/comp/Button';
 import SVGIcon from '@/comp/SVGIcon';
 import type { Theme } from '@/styles/theme';
@@ -9,7 +9,7 @@ interface LoginFormProps {
   onGitHubLogin: () => void;
 }
 
-export const LoginForm = ({ onGoogleLogin, onGitHubLogin }: LoginFormProps) => {
+export const LoginForm = memo(({ onGoogleLogin, onGitHubLogin }: LoginFormProps) => {
   const theme = useTheme();
 
   return (
@@ -40,7 +40,7 @@ export const LoginForm = ({ onGoogleLogin, onGitHubLogin }: LoginFormProps) => {
       </div>
     </div>
   );
-};
+});
 
 const containerStyle = () => css`
   min-height: 100vh;

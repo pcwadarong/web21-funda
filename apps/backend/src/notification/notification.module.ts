518f3345eb7d9b8bfaccbe54907d2fd22a7b9d23
Chaewon <pcwadarong@naver.com>
2026-01-21 02:59:59 +0900

    fix: 의존성으로 user repository 추가 - related to #205

--- apps/backend/src/notification/notification.module.ts
+++ apps/backend/src/notification/notification.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities';
 
 import { NotificationService } from './notification.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [NotificationService],
 })
 export class NotificationModule {}

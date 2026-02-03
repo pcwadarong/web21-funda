f02309e052b79bd9dbdf03933e7392d01d1464ce
gwang-coding <jaegwang1238@gmail.com>
2026-02-04 06:52:00 +0900

    feat: sentry 초기 설정

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 
 import { worker } from '@/mocks/browser';
@@ -6,6 +7,20 @@ import App from './app';
 
 import '@/styles/main.css';
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/funda\.website\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 const startApp = async () => {
   if (process.env.NODE_ENV === 'development' && import.meta.env.VITE_ENABLE_MSW === 'true') {
     await worker.start({ onUnhandledRequest: 'bypass' });
